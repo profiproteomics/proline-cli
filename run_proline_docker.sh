7846393086171118ef80d22759c5e8aa67d340f2
david-bouyssie <bouyssie@ipbs.fr>
2019-11-07 10:51:38 +0100

    Update script run_proline_docker.sh

--- run_proline_docker.sh
+++ run_proline_docker.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run --rm -v "$(pwd)/config":"/proline/config" -v "$(pwd)/proline_results":"/proline/proline_results" proline-cli:19.11.06
+docker run --rm -v "$(pwd)/config":"/proline/config" -v "$(pwd)/proline_results":"/proline/proline_results" dbouyssie/proline-cli:19.11.06
